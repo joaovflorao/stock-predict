43a0bc04ad08818683f00cdc8be856049b15babd
João Florão <joaovitor.florao@gmail.com>
2026-08-17 23:24:07 -0300

    feat(repositories): implementação dos métodos de interação com o DB

--- src/stock_predict/repositories/item_repository.py
+++ src/stock_predict/repositories/item_repository.py
@@ -8,14 +8,34 @@ class ItemRepository:
     def __init__(self, db: Session):
         self.db = db
 
-    def get_by_external_id(self) -> Item | None:
-        raise NotImplementedError
+    def get_by_external_id(self, external_id: str) -> Item | None:
+        return (
+            self.db.query(Item)
+            .filter(Item.external_id == external_id)
+            .first()
+        )
 
     def create(self, item: ItemCreate) -> Item:
-        raise NotImplementedError
+        db_item = Item(
+            external_id=item.external_id,
+            description=item.description,
+        )
+        self.db.add(db_item)
+        self.db.commit()
+        self.db.refresh(db_item)
+        return db_item
 
     def get_or_create(self, external_id: str, description: str) -> Item:
-        raise NotImplementedError
+        existing_item = self.get_by_external_id(external_id)
+        if existing_item:
+            return existing_item
+
+        return self.create(
+            ItemCreate(
+                external_id=external_id,
+                description=description,
+            )
+        )
 
     def list_all(self) -> list[Item]:
-        raise NotImplementedError
+        return self.db.query(Item).all()
--- src/stock_predict/repositories/movement_repository.py
+++ src/stock_predict/repositories/movement_repository.py
@@ -1,7 +1,7 @@
 from datetime import date
 from sqlalchemy.orm import Session
 
-from stock_predict.models.item import Movement
+from stock_predict.models.movement import Movement
 from stock_predict.schemas.movement import MovementCreate
 
 
@@ -10,10 +10,49 @@ class MovementRepository:
         self.db = db
 
     def create(self, movement: MovementCreate) -> Movement:
-        raise NotImplementedError
+        db_movement = Movement(
+            item_id=movement.item_id,
+            movement_date=movement.movement_date,
+            quantity=movement.quantity,
+            movement_type=movement.movement_type,
+        )
+        self.db.add(db_movement)
+        self.db.commit()
+        self.db.refresh(db_movement)
+        return db_movement
 
-    def bulk_create(self, movements: list[Movement]) -> None:
-        raise NotImplementedError
+    def bulk_create(self, movements: list[MovementCreate]) -> None:
+        db_movements_list = [
+            Movement(
+                item_id=move.item_id,
+                movement_date=move.movement_date,
+                quantity=move.quantity,
+                movement_type=move.movement_type,
+            )
+            for move in movements
+        ]
+        self.db.bulk_save_objects(db_movements_list)
+        self.db.commit()
 
-    def get_by_item(self, item_id: int, start_date: date = None, end_date: date = None) -> list[Movement]:
-        raise NotImplementedError
+    def get_by_item(
+            self,
+            item_id: int,
+            start_date: date | None = None,
+            end_date: date | None = None
+    ) -> list[Movement]:
+        db_movements_by_item = (
+            self.db.query(Movement)
+            .filter(Movement.item_id == item_id)
+        )
+        if start_date:
+            db_movements_by_item = db_movements_by_item.filter(
+                Movement.movement_date >= start_date
+            )
+        if end_date:
+            db_movements_by_item = db_movements_by_item.filter(
+                Movement.movement_date <= end_date
+            )
+
+        return db_movements_by_item.order_by(
+            Movement.movement_date
+        ).all()
